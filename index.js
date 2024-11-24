500025a0f97b154fbf939120c9cec91140d53277
Gulnar <gulnarmmdzd@gmail.com>
2024-11-24 22:43:35 +0400

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ valyuta1.querySelectorAll('button').forEach(btn => {
         converter().then(response => {
             inputbox2.value = response[second]
             document.querySelector('.input1 .input-wrapper .current-value').innerHTML = `1 ${first} = ${(response.rate).toFixed(5)} ${second}`
-            document.querySelector('.input2 .input-wrapper .current-value').innerHTML = `1 ${second} = ${(1/response.rate).toFixed(5)} ${first}`
-        })   
+            document.querySelector('.input2 .input-wrapper .current-value').innerHTML = `1 ${second} = ${(1 / response.rate).toFixed(5)} ${first}`
+        })
     })
 })
 
@@ -30,7 +30,7 @@ valyuta2.querySelectorAll('button').forEach(btn => {
         converter().then(response => {
             inputbox2.value = response[second]
             document.querySelector('.input1 .input-wrapper .current-value').innerHTML = `1 ${first} = ${(response.rate).toFixed(5)} ${second}`
-            document.querySelector('.input2 .input-wrapper .current-value').innerHTML = `1 ${second} = ${(1/response.rate).toFixed(5)} ${first}`
+            document.querySelector('.input2 .input-wrapper .current-value').innerHTML = `1 ${second} = ${(1 / response.rate).toFixed(5)} ${first}`
         })
     })
 })
@@ -39,40 +39,58 @@ inputbox1.addEventListener('input', e => {
 
     let value = inputbox1.value;
     value = value.replace(/,/g, '.');
-    value = value.replace(/[^0-9.]/g, ''); 
-    const parts = value.split('.'); 
+    value = value.replace(/[^0-9.]/g, '');
+    const parts = value.split('.');
     if (parts.length > 2) {
-        value = parts[0] + '.' + parts.slice(1).join(''); 
+        value = parts[0] + '.' + parts.slice(1).join('');
     }
-    inputbox1.value = value.slice(0,15);
+    if (value.charAt(0) == '.') {
+        value = `0${value}`
+    } else if (value.charAt(0) == '0' && !value.includes('.') && value.length > 1) {
+        value = value.slice(1)
+    }
 
-
-
-
+    let temp = value.slice(0, 15);
+    inputbox1.value = temp.includes('.') ? temp.split('.')[0] + '.' + temp.split('.')[1].slice(0, 5) : temp
     defaultAmount = e.target.value
     first = valyuta1.querySelector('.active').innerHTML
     second = valyuta2.querySelector('.active').innerHTML
-    converter().then(response => {
-        inputbox2.value = response[second]
-    })
+
+        converter().then(response => {
+            inputbox2.value = response[second]
+        })
+
+ 
+
+
+
+
+
+
 })
 
 inputbox2.addEventListener('input', e => {
 
     let value = inputbox2.value;
     value = value.replace(/,/g, '.');
-    value = value.replace(/[^0-9.]/g, ''); 
-    const parts = value.split('.'); 
+    value = value.replace(/[^0-9.]/g, '');
+    const parts = value.split('.');
     if (parts.length > 2) {
-        value = parts[0] + '.' + parts.slice(1).join(''); 
+        value = parts[0] + '.' + parts.slice(1).join('');
     }
-    inputbox2.value = value.slice(0,15);
+        if (value.charAt(0) == '.') {
+        value = `0${value}`
+    } else if (value.charAt(0) == '0' && !value.includes('.') && value.length > 1) {
+        value = value.slice(1)
+    }
+
+    let temp = value.slice(0, 15);
+    inputbox2.value = temp.includes('.') ? temp.split('.')[0] + '.' + temp.split('.')[1].slice(0, 5) : temp
 
     defaultAmount = e.target.value
     first = valyuta2.querySelector('.active').innerHTML
     second = valyuta1.querySelector('.active').innerHTML
     converter().then(response => {
-        console.log(response);      
         inputbox1.value = response[second]
     })
 })
@@ -84,7 +102,7 @@ const converter = async () => {
     }
 
     const response = await fetch(`https://api.fastforex.io/convert?from=${first}&to=${second}&amount=${defaultAmount}&precision=5&api_key=16e9275506-810eaaec51-sncoi4`).catch(error =>
-    document.querySelector("#result").innerText = "No internet connection"
+        document.querySelector("#result").innerText = "No internet connection"
 
     )
     const data = await response.json();
@@ -94,10 +112,10 @@ const converter = async () => {
 
 
 inputbox1.value = defaultAmount
-converter().then(r => {  
+converter().then(r => {
     inputbox2.value = r[second]
     document.querySelector('.input1 .input-wrapper .current-value').innerHTML = `1 ${first} = ${(r.rate).toFixed(5)} ${second}`
-                document.querySelector('.input2 .input-wrapper .current-value').innerHTML = `1 ${second} = ${(1/r.rate).toFixed(5)} ${first}`
+    document.querySelector('.input2 .input-wrapper .current-value').innerHTML = `1 ${second} = ${(1 / r.rate).toFixed(5)} ${first}`
 });
 
 
@@ -106,15 +124,15 @@ const item1 = document.querySelector(".item1-mobile");
 const close = document.querySelector("#close-btn");
 // const items = document.querySelectorAll(".item"); 
 
-item1.addEventListener('click', ()=> {
+item1.addEventListener('click', () => {
     document.querySelector('.menu-items').classList.toggle('hidden')
 })
-close.addEventListener('click', ()=> {
+close.addEventListener('click', () => {
     document.querySelector('.menu-items').classList.toggle('hidden')
 })
 
-window.addEventListener('resize', ()=> {
-    if(window.innerWidth > 992) {
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 992) {
         document.querySelector('.menu-items').classList.add('hidden')
     }
 })
